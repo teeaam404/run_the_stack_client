2f5088b3897db8f051c29d4dda9b922d2694c57c
RifatHossain41 <rifathossain0423@gmail.com>
2023-08-09 15:57:18 +0600

    added core value section

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,11 +1,13 @@
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
+import CoreValue from "./Components/CoreValue/CoreValue.jsx"
 
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
+      <CoreValue />
       <Footer />
     </div>
   );
