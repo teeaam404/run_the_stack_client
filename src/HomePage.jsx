66fff5676dc51f8f958271d9aa3e434233165b51
Sayhan Ahmed <121635376+SayhanA@users.noreply.github.com>
2023-08-10 18:28:47 +0600

    FAQ section added

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,3 +1,4 @@
+import Faq from "./Components/FAQ/Faq";
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 
@@ -6,6 +7,7 @@ const HomePage = () => {
   return (
     <div>
       <Navbar />
+      <Faq />
       <Footer />
     </div>
   );
