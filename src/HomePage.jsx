4c90745fbc3f19d4ac352ee7c401baec3208a09e
sourov-hossain1 <sourovhossain2122006@gmail.com>
2023-08-09 22:49:09 -0700

    banner section added

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,11 +1,13 @@
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
+import TopBanner from "./TopBanner/TopBanner";
 
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
+      <TopBanner></TopBanner>
       <Footer />
     </div>
   );
