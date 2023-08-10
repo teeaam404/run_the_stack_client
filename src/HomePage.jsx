0ffd28fb36446fd9b9296a1bbe46f132b0024abe
RifatHossain41 <rifathossain0423@gmail.com>
2023-08-10 16:21:45 +0600

    added core value section

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -2,6 +2,7 @@ import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import QnaSection from "./Components/QnaSection/QnaSection";
 import TopBanner from "./TopBanner/TopBanner";
+import CoreValue from "./Components/CoreValue/CoreValue"
 
 
 const HomePage = () => {
@@ -9,6 +10,7 @@ const HomePage = () => {
     <div>
       <Navbar />
       <TopBanner></TopBanner>
+      <CoreValue></CoreValue>
       <QnaSection />
       <Footer />
     </div>
