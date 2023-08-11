c8d9469cac93ea5a32e3ddca7ebe407ee2865da9
Sayhan Ahmed <121635376+SayhanA@users.noreply.github.com>
2023-08-11 12:02:49 +0600

    FAQ section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TopBanner from '../Banner/TopBanner';
+import Faq from '../../../Components/FAQ/Faq';
 
 const Home = () => {
     return (
         <div>
             <TopBanner></TopBanner>
+            <Faq />
         </div>
     );
 };
