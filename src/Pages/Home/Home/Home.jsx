dbdd2f52fcedfd9c36749b8e8127b632c9adec59
RifatHossain41 <rifathossain0423@gmail.com>
2023-08-16 06:59:12 +0600

    added to react fast marquee

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 
 import Banner from '../../../Components/Banner/Banner';
+import Carousel from '../../../Components/Carousel/Carousel';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Carousel></Carousel>
         </div>
     );
 };
